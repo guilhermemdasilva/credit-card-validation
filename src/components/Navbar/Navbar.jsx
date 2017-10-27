ed1ec7a1469b7069fdb0a962b52f0e1e033a4098
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2017-10-27 01:19:03 -0400

    Fixes #7 - Added Navbar Component
    * Added shallow test for it
    * Added a Throw handler for requestAnimationFrame

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <h1 className="logo">Symbio Sweden</h1>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
