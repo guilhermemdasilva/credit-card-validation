0ec70fc4694e6e22463243e105723bdeed2c8974
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2017-10-27 01:58:10 -0400

    Fixes #11 - Added Panel Component
    * Added shallow test for it also

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Panel from '../Panel/Panel';
 
 const Page = () => {
   return (
@@ -8,37 +9,7 @@ const Page = () => {
           <h1>Order now</h1>
         </div>
 
-        <div className="panel">
-          <div className="panel-body">
-            <form>
-              <div className="form-group">
-                <label>Credit card number:</label>
-                <div className="input-group">
-                  <input type="text" className="form-control" id="card" />
-                  <div className="input-group-addon" id="type" />
-                </div>
-              </div>
-              <div className="form-group">
-                <label>Name on card:</label>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="clearfix">
-                <div className="form-group form-group-mini">
-                  <label>Expiry date:</label>
-                  <input type="text" className="form-control" />
-                </div>
-                <div className="form-group form-group-mini">
-                  <label>CVV:</label>
-                  <input type="text" className="form-control" />
-                </div>
-              </div>
-              <br />
-              <p>
-                <button className="btn btn-primary">Submit</button>
-              </p>
-            </form>
-          </div>
-        </div>
+        <Panel />
       </div>
     </div>
   );
