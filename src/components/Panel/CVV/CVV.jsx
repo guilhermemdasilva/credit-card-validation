02a43c18def8f51fbb21299f8e09fe8b68aa0190
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2017-10-28 04:45:37 -0400

    Fixes #15 - Added mask on credit card number and CVV
    * implemented Luhn's algorithm for validity
    * added react-input-mask to deal with masks
    * classification of card type for validity and masks

--- src/components/Panel/CVV/CVV.jsx
+++ src/components/Panel/CVV/CVV.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import InputMask from 'react-input-mask';
+import PropTypes from 'prop-types';
+import CreditCardNumber from '../CreditCardNumber/CreditCardNumber';
 
-const CVV = () => {
-  return (
-    <div className="cvv">
-      <label>CVV:</label>
-      <input type="text" className="form-control" />
-    </div>
-  );
-};
+class CVV extends Component {
+  static propTypes = {
+    type: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mask: '999',
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.type === CreditCardNumber.AMEX) {
+      this.setState({ mask: '9999' });
+    } else {
+      this.setState({ mask: '999' });
+    }
+  }
+
+  render() {
+    const { mask } = this.state;
+    return (
+      <div className="cvv">
+        <label>CVV:</label>
+        <InputMask className="form-control" mask={mask} />
+      </div>
+    );
+  }
+}
 
 export default CVV;
